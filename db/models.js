76d9de5e6ed85dedac0facff2b61139463dc069b
zxfjd3g <258147149@qq.com>
2018-07-17 15:55:13 +0800

    update

---
+++ db/models.js
@@ -0,0 +1,50 @@
+/*
+能操作集合数据的model模块
+1. 连接数据库
+  1.1. 引入mongoose
+  1.2. 连接指定数据库(URL只有数据库是变化的)
+  1.3. 获取连接对象
+  1.4. 绑定连接完成的监听(用来提示连接成功)
+2. 定义出对应特定集合的Model并向外暴露
+  2.1. 字义Schema(描述文档结构)
+  2.2. 定义Model(与集合对应, 可以操作集合)
+  2.3. 向外暴露Model
+ */
+/*1. 连接数据库*/
+// 1.1. 引入mongoose
+const mongoose = require('mongoose')
+// 1.2. 连接指定数据库(URL只有数据库是变化的)
+mongoose.connect('mongodb://localhost:27017/180315_gzhipin')
+// 1.3. 获取连接对象
+const conn = mongoose.connection
+// 1.4. 绑定连接完成的监听(用来提示连接成功)
+conn.on('connected', function () {
+  console.log('db connect success!')
+})
+/*2. 定义出对应特定集合的Model并向外暴露*/
+// 2.1. 字义Schema(描述文档结构)
+const userSchema = mongoose.Schema({
+  username: {type: String, required: true}, // 用户名
+  password: {type: String, required: true}, // 密码
+  type: {type: String, required: true}, // 用户类型: dashen/laoban
+  header: {type: String}, // 头像名称
+  post: {type: String}, // 职位
+  info: {type: String}, // 个人或职位简介
+  company: {type: String}, // 公司名称
+  salary: {type: String} // 工资
+})
+// 2.2. 定义Model(与集合对应, 可以操作集合)
+const UserModel = mongoose.model('users', userSchema)
+// 2.3. 向外暴露Model
+//module.exports = UserModel
+exports.UserModel = UserModel
+
+/*
+module.exports = value
+exports.xxx = value1
+exports.yyy = value2
+ */
+
+/*
+commonjs模块化: 向外暴露的永远是exports的值, 默认值是一个空对象
+ */
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const md5 = require('blueimp-md5')
+
+const {UserModel} = require('../db/models')
+const filter = {password: 0, __v: 0} // 查询的过滤(去除文档中的指定属性)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +16,7 @@ router.get('/', function(req, res, next) {
 3. 返回响应
  */
 // 注册一个用户注册的路由接口
-router.post('/register', function (req, res) {
+/*router.post('/register', function (req, res) {
   // 1. 获取请求参数数据
   const {username, password} = req.body
   // 2. 处理数据
@@ -26,6 +30,62 @@ router.post('/register', function (req, res) {
     res.send({code: 0, data: user})
   }
 
+})*/
+
+// 注册的路由
+router.post('/register', function (req, res) {
+  // 1. 获取请求参数数据
+  const {username, password, type} = req.body
+  // 2. 处理数据
+    // 2.1. 根据username查询users集合中对应的user
+  UserModel.findOne({username}, function (error, user) {
+    // 2.2. 如果有, 说明已经存在,
+    if(user) {
+      // 3. 返回响应(失败)
+      res.send({
+        "code": 1,
+        "msg": "此用户已存在"
+      })
+    } else {
+      // 2.3. 如果没有, 保存到users中
+      new UserModel({username, password: md5(password), type}).save(function (error, user) {
+
+        // 向浏览器返回一个userid的cookie
+        res.cookie('userid', user._id, {maxAge: 1000*60*60*24*7})
+
+        // 3. 返回响应(成功)
+        res.send({
+          "code": 0,
+          "data": {username, type, _id: user._id}
+        })
+      })
+
+    }
+  })
+
 })
+
+// 登陆的路由
+router.post('/login', function (req, res) {
+  const {username, password} = req.body
+  // 根据username/password查询users中对应的user
+  UserModel.findOne({username, password: md5(password)}, filter, function (error, user) {
+    if(user) { // 有, 成功
+      // 向浏览器返回一个userid的cookie
+      res.cookie('userid', user._id, {maxAge: 1000*60*60*24*7})
+
+      res.send({
+        "code": 0,
+        "data": user
+      })
+    } else { // 没有, 失败
+      res.send({
+        "code": 1,
+        "msg": "用户名或密码错误"
+      })
+    }
+  })
+})
+
 
 module.exports = router;
