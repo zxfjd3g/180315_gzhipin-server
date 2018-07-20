981840852c3b155202442e5b49e7756ae7f5fda8
zxfjd3g <258147149@qq.com>
2018-07-20 19:34:24 +0800

    update

--- db/models.js
+++ db/models.js
@@ -39,6 +39,21 @@ const UserModel = mongoose.model('users', userSchema)
 //module.exports = UserModel
 exports.UserModel = UserModel
 
+
+// 定义chats集合的文档结构
+const chatSchema = mongoose.Schema({
+  from: {type: String, required: true}, // 发送用户的id
+  to: {type: String, required: true}, // 接收用户的id
+  chat_id: {type: String, required: true}, // from和to组成的字符串
+  content: {type: String, required: true}, // 内容
+  read: {type:Boolean, default: false}, // 标识是否已读
+  create_time: {type: Number} // 创建时间
+})
+// 定义能操作chats集合数据的Model
+const ChatModel = mongoose.model('chats', chatSchema)
+// 向外暴露Model
+exports.ChatModel = ChatModel
+
 /*
 module.exports = value
 exports.xxx = value1
---
+++ socketIO/index.js
@@ -0,0 +1,24 @@
+/*
+socketio服务器端
+ */
+const {ChatModel} = require('../db/models')
+module.exports = function (server) {
+  // 得到IO对象
+  const io = require('socket.io')(server)
+  // 监视浏览器与服务器的连接
+  io.on('connection', function (socket) { // socket代表服务器与某个浏览器的连接对象
+    console.log('浏览器连接上了')
+    // 绑定接收消息的监听(sendMsg)
+    socket.on('sendMsg', function ({content, from, to}) {
+      console.log('sendMsg', {content, from, to})
+      // 1. 保存到数据库集合(chats)
+      const chat_id = from + '_' + to
+      const create_time = Date.now()
+      new ChatModel({content, from, to, chat_id, create_time}).save(function (error, chatMsg) {
+        // 2. 全局发送消息给所有连接的浏览器(receiveMsg: chatMsg)
+        io.emit('receiveMsg', chatMsg)
+        console.log('receiveMsg', chatMsg)
+      })
+    })
+  })
+}
